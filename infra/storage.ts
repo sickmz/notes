7baf62e72c29fedcb895ef268aaa2488b0d1fed7
sickmz <matteodalessio95@gmail.com>
2024-12-06 23:38:05 +0000

    adding dynamodb

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,10 @@
 export const bucket = new sst.aws.Bucket("Uploads");
+
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo("Notes", {
+  fields: {
+    user_id: "string",
+    note_id: "string",
+  },
+  primaryIndex: { hashKey: "user_id", rangeKey: "note_id" },
+});
